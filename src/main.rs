1df096494a12bf1928a36b8fd1edfad0a8729c3e
curoli <oliverr@broadinstitute.org>
2024-09-26 16:30:41 -0400

    Accessing S3

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, World!")
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let config = aws_config::load_from_env().await;
+    let s3_client = aws_sdk_s3::Client::new(&config);
+    let result = s3_client.list_buckets().send().await?;
+    match result.buckets {
+        None => {
+            println!("No buckets found");
+        }
+        Some(buckets) => {
+            for bucket in buckets {
+                println!("Bucket: {}", bucket.name().unwrap_or("<no name>"));
+            }
+        }
+    };
+    Ok(())
 }
