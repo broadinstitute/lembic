c7652eaaa80cfd1f0d88726f539d92c41994eb0b
curoli <oliverr@broadinstitute.org>
2024-10-03 17:31:43 -0400

    Parsing data, part 1.

--- src/read.rs
+++ src/read.rs
@@ -1,3 +1,4 @@
+use tokio::io::AsyncBufReadExt;
 use crate::error::Error;
 use crate::runtime::Runtime;
 use crate::s3::S3Location;
@@ -5,18 +6,18 @@ use crate::s3::S3Location;
 pub(crate) fn read(runtime: &Runtime, location: &str) -> Result<(), Error> {
     let location = S3Location::try_from(location)?;
     println!("Reading object: {}", location);
-    let data =
-        runtime.tokio().block_on(async {
-            let response =
-                runtime.s3_client().get_object()
-                    .bucket(location.bucket())
-                    .key(location.key())
-                    .send()
-                    .await?;
-            let bytes = response.body.collect().await?.to_vec();
-            let string = String::from_utf8_lossy(&bytes).to_string();
-            Ok::<String, Error>(string)
-        })?;
-    println!("{}", data);
+    runtime.tokio().block_on(async {
+        let response =
+            runtime.s3_client().get_object()
+                .bucket(location.bucket())
+                .key(location.key())
+                .send()
+                .await?;
+        let mut lines = response.body.into_async_read().lines();
+        while let Some(line) = lines.next_line().await? {
+            println!("{}", line);
+        };
+        Ok::<(), Error>(())
+    })?;
     Ok(())
 }
